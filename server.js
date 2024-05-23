d368a1f4093c31cba7239f2b77c427aef303a759
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-05-23 14:53:38 -0400

    more route error fixes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
-app.use('/', routes);
+app.use('/', api);
 
 app.use(express.static('public'));
 
